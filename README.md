9bb9ea05de5d1b618324e6120a341024856dea63
moz667 <moz667@gmail.com>
2024-04-03 07:43:33 +0200

    init (2)

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # syncthing-archiver
 
 Archivador de imagenes almacenadas en las carpetas de stdversions de synchthing
+
+## Enlaces
+* https://docs.python.org/es/3/library/getopt.html
+* https://docs.python.org/3/library/configparser.html
+* https://pypi.org/project/pillow/
+
+## Environment (con pyenv)
+
+```bash
+pyenv virtualenv 3.7.2 syncthing-archiver
+echo "syncthing-archiver" > .python-version
+pip install --upgrade pip
+pip install -r requirements.txt
+```
+
+## TODO
+* [ ] Configuracion
+* [ ] Archivos .trash (verificar por si se borro antes de sincronizar)
+* [ ] Sqlite con info? Lo mismo puede ser interesante almacenar de alguna forma los sha1 de los archivos para validar duplicados o borrados (.trash)
+* [ ] Documentar como sincronizar
+    * [ ] Android
+    * [ ] IOS
--- src/synchthing-archiver.py
+++ src/synchthing-archiver.py
@@ -2,5 +2,49 @@
 # -*- coding: utf-8 -*-
 # Libraries
 
-# TODO: ALL
-pass
+import getopt, sys
+import configparser
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hc:v", ["help", "config="])
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+    
+    config_file = 'config.ini'
+    verbose = False
+    for o, a in opts:
+        if o == "-v":
+            verbose = True
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("-c", "--config"):
+            config_file = a
+        else:
+            assert False, "unhandled option"
+    
+    # TODO:
+    # Leer configuracion
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    for section_key in config.sections():
+        cfg_section = config[section_key]
+        stversions_folder = cfg_section['stversions_folder']
+        archive_folder = cfg_section['archive_folder']
+        move_files = True if not 'move_files' in cfg_section or cfg_section['move_files'] != 'false' else False
+        # Archivar
+        print(cfg_section)
+
+def usage():
+    # TODO:
+    print("TODO: Usage")
+
+def archive(source_folder, target_folder, move_files=True):
+    pass
+
+if __name__ == "__main__":
+    main()
+
